8ee4edf3d92d8137b055244b165a4c8f7d0571c0
lucas <lucaston2323@gmail.com>
2024-10-21 16:26:17 -0300

    feat: adicionando método para adicionar produto

--- src/br/com/pay/PayApplication.java
+++ src/br/com/pay/PayApplication.java
@@ -2,6 +2,7 @@ package br.com.pay;
 
 import br.com.pay.models.Account;
 import br.com.pay.models.Pay;
+import br.com.pay.models.Product;
 
 import java.util.Scanner;
 
@@ -24,13 +25,14 @@ public class PayApplication {
             """);
 
             menu = sc.nextInt();
+            sc.nextLine();
 
             switch (menu) {
                 case 1:
                     System.out.println("Limite atual: R$ " + account.getLimite());
                     break;
                 case 2:
-                    System.out.println("Nome do produto:");
+                    criandoProduto(sc, account);
                     break;
                 case 0:
                     System.out.println("Saindo do programa...");
@@ -39,5 +41,22 @@ public class PayApplication {
         } while (menu != 0);
         
         sc.close();
+    }
+
+    private  static  void criandoProduto(Scanner sc, Account account) {
+        System.out.println("Digite o nome do produto:");
+        String descricao = sc.nextLine();
+        System.out.println("Digite o valor do produto:");
+        double valor = sc.nextDouble();
+
+        Product product = new Product(descricao, valor);
+
+        boolean verificacao = account.validacaoCompra(product);
+
+        if (verificacao) {
+            System.out.println("Compra realizada!");
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
     }
 }
