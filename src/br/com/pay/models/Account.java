5d253675f970b805ebabb87c4e0bde2d1da147bf
lucas <lucaston2323@gmail.com>
2024-10-22 14:21:41 -0300

    refactor: integrando a class Pay ao account

--- src/br/com/pay/models/Account.java
+++ src/br/com/pay/models/Account.java
@@ -1,11 +1,15 @@
 package br.com.pay.models;
 
 public class Account {
-    Pay pay = new Pay();
+    private Pay pay = new Pay();
     private double limite;
 
     public double getLimite() {
         return limite;
+    }
+
+    public Pay getPay() {
+        return pay;
     }
 
     public Account(double limite) {
@@ -15,7 +19,7 @@ public class Account {
     public boolean validacaoCompra(Product product) {
         if (this.limite >= product.getValor()) {
             this.limite -= product.getValor();
-            pay.getProdutos().add(product);
+            this.pay.getProdutos().add(product);
             return true;
         }
         return false;
